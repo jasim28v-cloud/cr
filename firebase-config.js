e5f2f7067d56185405d5647e58e888287c279655
jasim28v-cloud <jasim28v@gmail.com>
2026-04-02 14:07:59 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// Firebase Configuration
 const firebaseConfig = {
     apiKey: "AIzaSyAxtEkrEgl0C9djPkxKKX-sENtOzPEbHB8",
     authDomain: "tope-e5350.firebaseapp.com",
@@ -10,23 +9,18 @@ const firebaseConfig = {
     measurementId: "G-YERBCEZEW9"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Initialize services
 const auth = firebase.auth();
 const db = firebase.database();
 const storage = firebase.storage();
 
-// Cloudinary Configuration
 const CLOUD_NAME = 'daemk3hut';
 const UPLOAD_PRESET = 'fok2_k';
 
-// Agora Configuration
 const AGORA_APP_ID_CALL = '929646610d814d529a06c4081c81325f';
 const AGORA_APP_ID_LIVE = '75d6c13a4f494ea8ad181eb55b641b79';
 
-// Admin Configuration
 const ADMIN_EMAIL = 'jasim28v@gmail.com';
 const ADMIN_PASSWORD = 'vv2314vv';
 
